e52db90c3cc4b57b4d6a30470bdf379dfee151d5
Ian Lee <IanLee1521@gmail.com>
2014-09-24 01:02:30 -0700

    pep8-ify source code (excluding test_main.py) and add to Travis and tox scripts

--- pygeoif/__init__.py
+++ pygeoif/__init__.py
@@ -12,14 +12,11 @@
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #   Lesser General Public License for more details.
 
-#   You should have received a copy of the GNU Lesser General Public
-#   License along with this library; if not, write to the Free Software
-#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this library; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 from .geometry import Point, LineString, LinearRing, Polygon
 from .geometry import MultiPoint, MultiLineString, MultiPolygon
 from .geometry import GeometryCollection
 from .geometry import as_shape, from_wkt, mapping, orient, signed_area
-
-
-
