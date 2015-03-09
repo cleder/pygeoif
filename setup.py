2854cbcb72a4fdc38c2e9b43870b6de5a876b6c7
nrwort4711 <nrwort4711@ung.edu>
2015-03-09 11:43:34 -0400

    PEP8 style review
    Made style changes based on PEP8 suggestions.  PEP8 still gives some warnings for docstring examples.

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 from setuptools.command.test import test as TestCommand
-import sys, os
+import sys
+import os
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
@@ -15,12 +19,13 @@ class PyTest(TestCommand):
 
 version = '0.5'
 
+README = open("README.rst").read()
+HISTORY = open(os.path.join("docs", "HISTORY.txt")).read()
+
 setup(name='pygeoif',
       version=version,
       description="A basic implementation of the __geo_interface__",
-            long_description=open(
-              "README.rst").read() + "\n" +
-              open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=README + "\n" + HISTORY,
       classifiers=[
         "Topic :: Scientific/Engineering :: GIS",
         "Programming Language :: Python",
@@ -37,7 +42,8 @@ setup(name='pygeoif',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Development Status :: 5 - Production/Stable',
         'Operating System :: OS Independent',
-      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      ],
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='GIS Spatial WKT',
       author='Christian Ledermann',
       author_email='christian.ledermann@gmail.com',
@@ -47,7 +53,7 @@ setup(name='pygeoif',
       include_package_data=True,
       zip_safe=False,
       tests_require=['pytest'],
-      cmdclass = {'test': PyTest},
+      cmdclass={'test': PyTest},
       install_requires=[
           # -*- Extra requirements: -*-
       ],
