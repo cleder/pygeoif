ba319c3f2002584d0238e1e65d079eda3fcc05c0
Christian Ledermann <christian.ledermann@gmail.com>
2021-09-17 12:20:44 +0100

    convert unittests to pytest with pytestify

--- pygeoif/tests/test_factories.py
+++ pygeoif/tests/test_factories.py
@@ -1,6 +1,6 @@
 """Test the geometry factories."""
 
-import unittest
+import pytest
 
 from pygeoif import factories
 from pygeoif import geometry
@@ -79,7 +79,7 @@ def test_shell_holes_from_wkt_coords():
     assert shell == [(0.0, 0.0), (10.0, 20.0), (30.0, 40.0), (0.0, 0.0)]
 
 
-class WKTTestCase(unittest.TestCase):
+class TestWKT:
 
     # valid and supported WKTs
     wkt_ok = [
@@ -109,29 +109,28 @@ class WKTTestCase(unittest.TestCase):
     def test_point(self):
         p = factories.from_wkt("POINT (0.0 1.0)")
         assert isinstance(p, geometry.Point)
-        self.assertEqual(p.x, 0.0)
-        self.assertEqual(p.y, 1.0)
-        self.assertEqual(p.wkt, "POINT (0.0 1.0)")
-        self.assertEqual(str(p), "POINT (0.0 1.0)")
-        self.assertEqual(p.geom_type, "Point")
+        assert p.x == 0.0
+        assert p.y == 1.0
+        assert p.wkt == "POINT (0.0 1.0)"
+        assert str(p) == "POINT (0.0 1.0)"
+        assert p.geom_type == "Point"
 
     def test_linestring(self):
         line = factories.from_wkt(
             "LINESTRING(-72.991 46.177,-73.079 46.16,"
             "-73.146 46.124,-73.177 46.071,-73.164 46.044)",
         )
-        self.assertEqual(
-            line.wkt,
-            "LINESTRING (-72.991 46.177, "
+        assert (
+            line.wkt == "LINESTRING (-72.991 46.177, "
             "-73.079 46.16, -73.146 46.124, "
-            "-73.177 46.071, -73.164 46.044)",
+            "-73.177 46.071, -73.164 46.044)"
         )
         assert isinstance(line, geometry.LineString)
 
     def test_linearring(self):
         r = factories.from_wkt("LINEARRING (0 0,0 1,1 0,0 0)")
-        self.assertEqual(isinstance(r, geometry.LinearRing), True)
-        self.assertEqual(r.wkt, "LINEARRING (0.0 0.0, 0.0 1.0, " "1.0 0.0, 0.0 0.0)")
+        assert isinstance(r, geometry.LinearRing)
+        assert r.wkt == "LINEARRING (0.0 0.0, 0.0 1.0, " "1.0 0.0, 0.0 0.0)"
 
     def test_polygon(self):
         p = factories.from_wkt(
@@ -141,59 +140,60 @@ class WKTTestCase(unittest.TestCase):
             "-91.638 76.202,-91.647 76.211,-91.648 76.218,"
             "-91.643 76.221,-91.636 76.222,-91.611 76.227))",
         )
-        self.assertEqual(p.exterior.coords[0][0], -91.611)
-        self.assertEqual(p.exterior.coords[0], p.exterior.coords[-1])
-        self.assertEqual(len(p.exterior.coords), 14)
+        assert p.exterior.coords[0][0] == -91.611
+        assert p.exterior.coords[0] == p.exterior.coords[-1]
+        assert len(p.exterior.coords) == 14
         p = factories.from_wkt(
             "POLYGON((1 1,5 1,5 5,1 5,1 1),(2 2, 3 2, 3 3, 2 3,2 2))",
         )
-        self.assertEqual(p.exterior.coords[0], p.exterior.coords[-1])
-        self.assertEqual(p.exterior.coords[0], (1.0, 1.0))
-        self.assertEqual(len(list(p.interiors)), 1)
-        self.assertEqual(
-            list(p.interiors)[0].coords,
-            ((2.0, 2.0), (3.0, 2.0), (3.0, 3.0), (2.0, 3.0), (2.0, 2.0)),
+        assert p.exterior.coords[0] == p.exterior.coords[-1]
+        assert p.exterior.coords[0] == (1.0, 1.0)
+        assert len(list(p.interiors)) == 1
+        assert list(p.interiors)[0].coords == (
+            (2.0, 2.0),
+            (3.0, 2.0),
+            (3.0, 3.0),
+            (2.0, 3.0),
+            (2.0, 2.0),
         )
-        self.assertEqual(
-            p.wkt,
-            "POLYGON ((1.0 1.0, 5.0 1.0, 5.0 5.0, "
+        assert (
+            p.wkt == "POLYGON ((1.0 1.0, 5.0 1.0, 5.0 5.0, "
             "1.0 5.0, 1.0 1.0),(2.0 2.0, 3.0 2.0, "
-            "3.0 3.0, 2.0 3.0, 2.0 2.0))",
+            "3.0 3.0, 2.0 3.0, 2.0 2.0))"
         )
         p = factories.from_wkt("POLYGON ((30 10, 10 20, 20 40, 40 40, 30 10))")
-        self.assertEqual(p.exterior.coords[0], p.exterior.coords[-1])
+        assert p.exterior.coords[0] == p.exterior.coords[-1]
         p = factories.from_wkt(
             """POLYGON ((35 10, 10 20, 15 40, 45 45, 35 10),
             (20 30, 35 35, 30 20, 20 30))""",
         )
-        self.assertEqual(p.exterior.coords[0], p.exterior.coords[-1])
+        assert p.exterior.coords[0] == p.exterior.coords[-1]
 
     def test_multipoint(self):
         p = factories.from_wkt("MULTIPOINT(3.5 5.6,4.8 10.5)")
-        self.assertEqual(isinstance(p, geometry.MultiPoint), True)
-        self.assertEqual(list(p.geoms)[0].x, 3.5)
-        self.assertEqual(list(p.geoms)[1].y, 10.5)
-        self.assertEqual(p.wkt, "MULTIPOINT(3.5 5.6, 4.8 10.5)")
+        assert isinstance(p, geometry.MultiPoint)
+        assert list(p.geoms)[0].x == 3.5
+        assert list(p.geoms)[1].y == 10.5
+        assert p.wkt == "MULTIPOINT(3.5 5.6, 4.8 10.5)"
         p = factories.from_wkt("MULTIPOINT ((10 40), (40 30), " "(20 20), (30 10))")
-        self.assertEqual(isinstance(p, geometry.MultiPoint), True)
-        self.assertEqual(list(p.geoms)[0].x, 10.0)
-        self.assertEqual(list(p.geoms)[3].y, 10.0)
+        assert isinstance(p, geometry.MultiPoint)
+        assert list(p.geoms)[0].x == 10.0
+        assert list(p.geoms)[3].y == 10.0
         p = factories.from_wkt("MULTIPOINT (10 40, 40 30, 20 20, 30 10)")
-        self.assertEqual(isinstance(p, geometry.MultiPoint), True)
-        self.assertEqual(list(p.geoms)[0].x, 10.0)
-        self.assertEqual(list(p.geoms)[3].y, 10.0)
+        assert isinstance(p, geometry.MultiPoint)
+        assert list(p.geoms)[0].x == 10.0
+        assert list(p.geoms)[3].y == 10.0
 
     def test_multilinestring(self):
         p = factories.from_wkt(
             "MULTILINESTRING((3 4,10 50,20 25)," "(-5 -8,-10 -8,-15 -4))",
         )
-        self.assertEqual(list(p.geoms)[0].coords, (((3, 4), (10, 50), (20, 25))))
-        self.assertEqual(list(p.geoms)[1].coords, (((-5, -8), (-10, -8), (-15, -4))))
-        self.assertEqual(
-            p.wkt,
-            "MULTILINESTRING((3.0 4.0, 10.0 50.0, "
+        assert list(p.geoms)[0].coords == (((3, 4), (10, 50), (20, 25)))
+        assert list(p.geoms)[1].coords == (((-5, -8), (-10, -8), (-15, -4)))
+        assert (
+            p.wkt == "MULTILINESTRING((3.0 4.0, 10.0 50.0, "
             "20.0 25.0),(-5.0 -8.0, "
-            "-10.0 -8.0, -15.0 -4.0))",
+            "-10.0 -8.0, -15.0 -4.0))"
         )
         p = factories.from_wkt(
             """MULTILINESTRING ((10 10, 20 20, 10 40),
@@ -207,66 +207,68 @@ class WKTTestCase(unittest.TestCase):
             "((100 100,110 110,120 120,100 100)))",
         )
         # two polygons: the first one has an interior ring
-        self.assertEqual(len(list(p.geoms)), 2)
-        self.assertEqual(
-            list(p.geoms)[0].exterior.coords,
-            ((0.0, 0.0), (10.0, 20.0), (30.0, 40.0), (0.0, 0.0)),
+        assert len(list(p.geoms)) == 2
+        assert list(p.geoms)[0].exterior.coords == (
+            (0.0, 0.0),
+            (10.0, 20.0),
+            (30.0, 40.0),
+            (0.0, 0.0),
         )
-        self.assertEqual(
-            list(list(p.geoms)[0].interiors)[0].coords,
-            ((1.0, 1.0), (2.0, 2.0), (3.0, 3.0), (1.0, 1.0)),
+        assert list(list(p.geoms)[0].interiors)[0].coords == (
+            (1.0, 1.0),
+            (2.0, 2.0),
+            (3.0, 3.0),
+            (1.0, 1.0),
         )
-        self.assertEqual(
-            list(p.geoms)[1].exterior.coords,
-            ((100.0, 100.0), (110.0, 110.0), (120.0, 120.0), (100.0, 100.0)),
+        assert list(p.geoms)[1].exterior.coords == (
+            (100.0, 100.0),
+            (110.0, 110.0),
+            (120.0, 120.0),
+            (100.0, 100.0),
         )
-        self.assertEqual(
-            p.wkt,
-            "MULTIPOLYGON(((0.0 0.0, 10.0 20.0, "
+        assert (
+            p.wkt == "MULTIPOLYGON(((0.0 0.0, 10.0 20.0, "
             "30.0 40.0, 0.0 0.0),"
             "(1.0 1.0, 2.0 2.0, 3.0 3.0, 1.0 1.0)),"
             "((100.0 100.0, 110.0 110.0,"
-            " 120.0 120.0, 100.0 100.0)))",
+            " 120.0 120.0, 100.0 100.0)))"
         )
         p = factories.from_wkt(
             "MULTIPOLYGON(((1 1,5 1,5 5,1 5,1 1),"
             "(2 2, 3 2, 3 3, 2 3,2 2)),((3 3,6 2,6 4,3 3)))",
         )
-        self.assertEqual(len(list(p.geoms)), 2)
+        assert len(list(p.geoms)) == 2
         p = factories.from_wkt(
             "MULTIPOLYGON (((30 20, 10 40, 45 40, 30 20)),"
             "((15 5, 40 10, 10 20, 5 10, 15 5)))",
         )
-        self.assertEqual(
-            p.__geo_interface__,
-            {
-                "type": "MultiPolygon",
-                "bbox": (5.0, 5.0, 45.0, 40.0),
-                "coordinates": (
-                    (((30.0, 20.0), (10.0, 40.0), (45.0, 40.0), (30.0, 20.0)),),
+        assert p.__geo_interface__ == {
+            "type": "MultiPolygon",
+            "bbox": (5.0, 5.0, 45.0, 40.0),
+            "coordinates": (
+                (((30.0, 20.0), (10.0, 40.0), (45.0, 40.0), (30.0, 20.0)),),
+                (
                     (
-                        (
-                            (15.0, 5.0),
-                            (40.0, 10.0),
-                            (10.0, 20.0),
-                            (5.0, 10.0),
-                            (15.0, 5.0),
-                        ),
+                        (15.0, 5.0),
+                        (40.0, 10.0),
+                        (10.0, 20.0),
+                        (5.0, 10.0),
+                        (15.0, 5.0),
                     ),
                 ),
-            },
-        )
+            ),
+        }
 
     def test_geometrycollection(self):
         gc = factories.from_wkt(
             "GEOMETRYCOLLECTION(POINT(4 6), LINESTRING(4 6,7 10))",
         )
-        self.assertEqual(len(list(gc.geoms)), 2)
-        self.assertIsInstance(list(gc.geoms)[0], geometry.Point)
-        self.assertIsInstance(list(gc.geoms)[1], geometry.LineString)
-        self.assertEqual(
-            gc.wkt,
-            "GEOMETRYCOLLECTION(POINT (4.0 6.0), LINESTRING (4.0 6.0, 7.0 10.0))",
+        assert len(list(gc.geoms)) == 2
+        assert isinstance(list(gc.geoms)[0], geometry.Point)
+        assert isinstance(list(gc.geoms)[1], geometry.LineString)
+        assert (
+            gc.wkt
+            == "GEOMETRYCOLLECTION(POINT (4.0 6.0), LINESTRING (4.0 6.0, 7.0 10.0))"
         )
 
     def test_wkt_ok(self):
@@ -275,55 +277,55 @@ class WKTTestCase(unittest.TestCase):
 
     def test_wkt_fail(self):
         for wkt in self.wkt_fail:
-            self.assertRaises(factories.WKTParserError, factories.from_wkt, wkt)
+            pytest.raises(factories.WKTParserError, factories.from_wkt, wkt)
 
     def test_wkt_tin(self):
         tin = "TIN (((0 0 0, 0 0 1, 0 1 0, 0 0 0)), ((0 0 0, 0 1 0, 1 1 0, 0 0 0)))"
-        self.assertRaises(factories.WKTParserError, factories.from_wkt, tin)
+        pytest.raises(factories.WKTParserError, factories.from_wkt, tin)
 
 
-class AsShapeTestCase(unittest.TestCase):
+class TestAsShape:
     def test_point(self):
         f = geometry.Point(0, 1)
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_linestring(self):
         f = geometry.LineString([(0, 0), (1, 1)])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_linearring(self):
         f = geometry.LinearRing([(0, 0), (1, 1), (1, 0), (0, 0)])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_polygon(self):
         f = geometry.Polygon([(0, 0), (1, 1), (1, 0), (0, 0)])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
         e = [(0, 0), (0, 2), (2, 2), (2, 0), (0, 0)]
         i = [(1, 0), (0.5, 0.5), (1, 1), (1.5, 0.5), (1, 0)]
         f = geometry.Polygon(e, [i])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
         ext = [(0, 0), (0, 2), (2, 2), (2, 0), (0, 0)]
         int_1 = [(0.5, 0.25), (1.5, 0.25), (1.5, 1.25), (0.5, 1.25), (0.5, 0.25)]
         int_2 = [(0.5, 1.25), (1, 1.25), (1, 1.75), (0.5, 1.75), (0.5, 1.25)]
         f = geometry.Polygon(ext, [int_1, int_2])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_multipoint(self):
         f = geometry.MultiPoint([[0.0, 0.0], [1.0, 2.0]])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_multilinestring(self):
         f = geometry.MultiLineString([[[0.0, 0.0], [1.0, 2.0]]])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
-        self.assertEqual((0, 0, 1, 2), f.bounds)
+        assert f.__geo_interface__ == s.__geo_interface__
+        assert (0, 0, 1, 2) == f.bounds
 
     def test_multipolygon(self):
         f = geometry.MultiPolygon(
@@ -335,28 +337,28 @@ class AsShapeTestCase(unittest.TestCase):
             ],
         )
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
 
     def test_geometrycollection(self):
         p = geometry.Point(0, 1)
         line = geometry.LineString([(0, 0), (1, 1)])
         f = geometry.GeometryCollection([p, line])
         s = factories.shape(f)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
-        self.assertEqual(f.__geo_interface__["geometries"][0], p.__geo_interface__)
-        self.assertEqual(f.__geo_interface__["geometries"][1], line.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
+        assert f.__geo_interface__["geometries"][0] == p.__geo_interface__
+        assert f.__geo_interface__["geometries"][1] == line.__geo_interface__
 
     def test_nongeo(self):
-        self.assertRaises(AttributeError, factories.shape, "a")
+        pytest.raises(AttributeError, factories.shape, "a")
 
     def test_empty_dict(self):
-        self.assertRaises(TypeError, factories.shape, {})
+        pytest.raises(TypeError, factories.shape, {})
 
     def test_notimplemented_interface(self):
         f = {"type": "Tin", "geometries": (1, 2, 3)}
-        self.assertRaises(NotImplementedError, factories.shape, f)
+        pytest.raises(NotImplementedError, factories.shape, f)
 
     def test_dict_as_shape(self):
         f = geometry.MultiLineString([[[0.0, 0.0], [1.0, 2.0]]])
         s = factories.shape(f.__geo_interface__)
-        self.assertEqual(f.__geo_interface__, s.__geo_interface__)
+        assert f.__geo_interface__ == s.__geo_interface__
--- pygeoif/tests/test_feature.py
+++ pygeoif/tests/test_feature.py
@@ -1,12 +1,14 @@
 """Test Feature and FeatureCollection."""
 import unittest
 
+import pytest
+
 from pygeoif import feature
 from pygeoif import geometry
 
 
-class FeatureTestCase(unittest.TestCase):
-    def setUp(self):
+class TestFeature:
+    def setup_method(self):
         self.a = geometry.Polygon(
             ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
         )
@@ -19,185 +21,164 @@ class FeatureTestCase(unittest.TestCase):
         self.fc = feature.FeatureCollection([self.f1, self.f2])
 
     def test_feature_eq(self):
-        self.assertEqual(self.f1, feature.Feature(self.a))
-        self.assertEqual(self.f3, feature.Feature(self.a, {}, feature_id="1"))
+        assert self.f1 == feature.Feature(self.a)
+        assert self.f3 == feature.Feature(self.a, {}, feature_id="1")
 
     def test_feature_neq_no_geo_interface(self):
-        self.assertNotEqual(self.f1, None)
+        assert self.f1 != object()
 
     def test_feature_neq_no_geo_interface_geometry(self):
-        self.assertNotEqual(self.f1, unittest.mock.Mock(__geo_interface__={}))
+        assert self.f1 != unittest.mock.Mock(__geo_interface__={})
 
     def test_feature_neq_no_geo_interface_type(self):
-        self.assertNotEqual(
-            self.f1,
-            unittest.mock.Mock(__geo_interface__={"type": "foo"}),
-        )
+        assert self.f1 != unittest.mock.Mock(__geo_interface__={"type": "foo"})
 
     def test_feature_neq_no_geo_interface_features(self):
-        self.assertNotEqual(
-            self.f1,
-            unittest.mock.Mock(__geo_interface__={"type": "Feature"}),
-        )
+        assert self.f1 != unittest.mock.Mock(__geo_interface__={"type": "Feature"})
 
     def test_feature(self):
-        self.assertRaises(TypeError, feature.Feature)
-        self.assertEqual(
-            self.f1.__geo_interface__,
-            {
-                "type": "Feature",
+        pytest.raises(TypeError, feature.Feature)
+        assert self.f1.__geo_interface__ == {
+            "type": "Feature",
+            "bbox": (0.0, 0.0, 1.0, 1.0),
+            "geometry": {
+                "type": "Polygon",
                 "bbox": (0.0, 0.0, 1.0, 1.0),
-                "geometry": {
-                    "type": "Polygon",
-                    "bbox": (0.0, 0.0, 1.0, 1.0),
-                    "coordinates": (
-                        ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
-                    ),
-                },
-                "properties": {},
+                "coordinates": (
+                    ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
+                ),
             },
-        )
+            "properties": {},
+        }
         self.f1.properties["coords"] = {"cube": (0, 0, 0)}
-        self.assertEqual(
-            self.f1.__geo_interface__,
-            {
-                "type": "Feature",
+        assert self.f1.__geo_interface__ == {
+            "type": "Feature",
+            "bbox": (0.0, 0.0, 1.0, 1.0),
+            "geometry": {
+                "type": "Polygon",
                 "bbox": (0.0, 0.0, 1.0, 1.0),
-                "geometry": {
-                    "type": "Polygon",
-                    "bbox": (0.0, 0.0, 1.0, 1.0),
-                    "coordinates": (
-                        ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
-                    ),
-                },
-                "properties": {"coords": {"cube": (0, 0, 0)}},
+                "coordinates": (
+                    ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
+                ),
             },
-        )
-        self.assertEqual(self.f1.geometry.bounds, (0.0, 0.0, 1.0, 1.0))
+            "properties": {"coords": {"cube": (0, 0, 0)}},
+        }
+        assert self.f1.geometry.bounds == (0.0, 0.0, 1.0, 1.0)
         del self.f1.properties["coords"]
 
     def test_feature_with_id(self):
-        self.assertEqual(self.f3.id, "1")
-        self.assertEqual(
-            self.f3.__geo_interface__,
-            {
-                "type": "Feature",
+        assert self.f3.id == "1"
+        assert self.f3.__geo_interface__ == {
+            "type": "Feature",
+            "bbox": (0.0, 0.0, 1.0, 1.0),
+            "geometry": {
+                "type": "Polygon",
                 "bbox": (0.0, 0.0, 1.0, 1.0),
-                "geometry": {
-                    "type": "Polygon",
-                    "bbox": (0.0, 0.0, 1.0, 1.0),
-                    "coordinates": (
-                        ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
-                    ),
-                },
-                "id": "1",
-                "properties": {},
+                "coordinates": (
+                    ((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),
+                ),
             },
-        )
+            "id": "1",
+            "properties": {},
+        }
 
     def test_feature_repr(self):
-        self.assertEqual(
-            repr(self.f3),
-            "Feature("  # noqa: P103
+        assert (
+            repr(self.f3) == "Feature("
             "Polygon(((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),),"
-            " {}, '1')",
+            " {}, '1')"
         )
 
     def test_feature_repr_eval(self):
-        self.assertEqual(
+        assert (
             eval(
                 repr(self.f2),
                 {},
                 {"Polygon": geometry.Polygon, "Feature": feature.Feature},
-            ).__geo_interface__,
-            self.f2.__geo_interface__,
+            ).__geo_interface__
+            == self.f2.__geo_interface__
         )
 
     def test_featurecollection(self):
-        self.assertRaises(TypeError, feature.FeatureCollection)
-        self.assertRaises(TypeError, feature.FeatureCollection, None)
-        self.assertEqual(len(list(self.fc.features)), 2)
-        self.assertEqual(len(self.fc), 2)
-        self.assertEqual(self.fc.bounds, (0.0, 0.0, 2.0, 2.0))
-        self.assertEqual([self.f1, self.f2], list(self.fc))
-        self.assertEqual(
-            self.fc.__geo_interface__,
-            {
-                "bbox": (0.0, 0.0, 2.0, 2.0),
-                "features": (
-                    {
+        pytest.raises(TypeError, feature.FeatureCollection)
+        pytest.raises(TypeError, feature.FeatureCollection, None)
+        assert len(list(self.fc.features)) == 2
+        assert len(self.fc) == 2
+        assert self.fc.bounds == (0.0, 0.0, 2.0, 2.0)
+        assert [self.f1, self.f2] == list(self.fc)
+        assert self.fc.__geo_interface__ == {
+            "bbox": (0.0, 0.0, 2.0, 2.0),
+            "features": (
+                {
+                    "bbox": (0.0, 0.0, 1.0, 1.0),
+                    "geometry": {
                         "bbox": (0.0, 0.0, 1.0, 1.0),
-                        "geometry": {
-                            "bbox": (0.0, 0.0, 1.0, 1.0),
-                            "coordinates": (
-                                (
-                                    (0.0, 0.0),
-                                    (0.0, 1.0),
-                                    (1.0, 1.0),
-                                    (1.0, 0.0),
-                                    (0.0, 0.0),
-                                ),
+                        "coordinates": (
+                            (
+                                (0.0, 0.0),
+                                (0.0, 1.0),
+                                (1.0, 1.0),
+                                (1.0, 0.0),
+                                (0.0, 0.0),
                             ),
-                            "type": "Polygon",
-                        },
-                        "properties": {},
-                        "type": "Feature",
+                        ),
+                        "type": "Polygon",
                     },
-                    {
+                    "properties": {},
+                    "type": "Feature",
+                },
+                {
+                    "bbox": (0.0, 0.0, 2.0, 2.0),
+                    "geometry": {
                         "bbox": (0.0, 0.0, 2.0, 2.0),
-                        "geometry": {
-                            "bbox": (0.0, 0.0, 2.0, 2.0),
-                            "coordinates": (
-                                (
-                                    (0.0, 0.0),
-                                    (0.0, 2.0),
-                                    (2.0, 1.0),
-                                    (2.0, 0.0),
-                                    (0.0, 0.0),
-                                ),
+                        "coordinates": (
+                            (
+                                (0.0, 0.0),
+                                (0.0, 2.0),
+                                (2.0, 1.0),
+                                (2.0, 0.0),
+                                (0.0, 0.0),
                             ),
-                            "type": "Polygon",
-                        },
-                        "properties": {},
-                        "type": "Feature",
+                        ),
+                        "type": "Polygon",
                     },
-                ),
-                "type": "FeatureCollection",
-            },
-        )
+                    "properties": {},
+                    "type": "Feature",
+                },
+            ),
+            "type": "FeatureCollection",
+        }
 
     def test_featurecollection_eq(self):
-        self.assertEqual(self.fc, feature.FeatureCollection([self.f1, self.f2]))
+        assert self.fc == feature.FeatureCollection([self.f1, self.f2])
 
     def test_featurecollection_neq_no_geo_interface(self):
-        self.assertNotEqual(self.fc, None)
+        assert self.fc != object()
 
     def test_featurecollection_neq_no_geo_interface_geometry(self):
-        self.assertNotEqual(self.fc, unittest.mock.Mock(__geo_interface__={}))
+        assert self.fc != unittest.mock.Mock(__geo_interface__={})
 
     def test_featurecollection_neq_no_geo_interface_features(self):
-        self.assertNotEqual(
-            self.fc,
-            unittest.mock.Mock(__geo_interface__={"type": "FeatureCollection"}),
+        assert self.fc != unittest.mock.Mock(
+            __geo_interface__={"type": "FeatureCollection"},
         )
 
     def test_featurecollection_neq_no_geo_interface_len_features(self):
-        self.assertNotEqual(self.fc, feature.FeatureCollection([self.f1]))
+        assert self.fc != feature.FeatureCollection([self.f1])
 
     def test_featurecollection_repr(self):
-        self.assertEqual(
-            repr(self.fc),
-            "FeatureCollection("  # noqa: P103
+        assert (
+            repr(self.fc) == "FeatureCollection("
             "(Feature("
             "Polygon(((0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)),),"
             " {}, None), "
             "Feature("
             "Polygon(((0.0, 0.0), (0.0, 2.0), (2.0, 1.0), (2.0, 0.0), (0.0, 0.0)),), "
-            "{}, None)))",
+            "{}, None)))"
         )
 
     def test_featurecollection_repr_eval(self):
-        self.assertEqual(
+        assert (
             eval(
                 repr(self.fc),
                 {},
@@ -206,8 +187,8 @@ class FeatureTestCase(unittest.TestCase):
                     "Feature": feature.Feature,
                     "FeatureCollection": feature.FeatureCollection,
                 },
-            ).__geo_interface__,
-            self.fc.__geo_interface__,
+            ).__geo_interface__
+            == self.fc.__geo_interface__
         )
 
     def test_featurecollection_bounds(self):
@@ -215,4 +196,4 @@ class FeatureTestCase(unittest.TestCase):
         ls2 = geometry.LineString(((2, 3), (3, 4)))
         fc = feature.FeatureCollection([feature.Feature(ls1), feature.Feature(ls2)])
 
-        self.assertEqual(fc.bounds, (0, 1, 3, 4))
+        assert fc.bounds == (0, 1, 3, 4)
