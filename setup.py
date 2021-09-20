e5a0070c436a135bb2959427c815d338ca159a10
Christian Ledermann <christian.ledermann@gmail.com>
2021-09-20 22:57:32 +0100

    beta 1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-version = "1.0.beta.0"
+version = "1.0.beta.1"
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.txt")).read()
