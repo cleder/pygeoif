37bacf86808428738847f8ecb717ab1a4d0aadfe
Christian Ledermann <christian.ledermann@gmail.com>
2021-09-21 02:17:13 +0100

    black

--- pygeoif/geometry.py
+++ pygeoif/geometry.py
@@ -77,7 +77,10 @@ class _Geometry:
         for 1, to a Point.
         """
         if self.has_z:
-            warnings.warn("The convex Hull will only return the projection to 2 dimensions xy coordinates")
+            warnings.warn(
+                "The convex Hull will only return the projection "
+                "to 2 dimensions xy coordinates",
+            )
         hull = convex_hull(self._prepare_hull())
         if len(hull) == 0:
             return None
