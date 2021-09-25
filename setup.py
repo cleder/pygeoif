f200aa5355c348a3fd2c878cc0be4024ade525c4
Christian Ledermann <christian.ledermann@gmail.com>
2021-09-25 11:43:55 +0100

    flake8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Development Status :: 4 - Beta",
-        #"Development Status :: 5 - Production/Stable",
+        # "Development Status :: 5 - Production/Stable",
         "Operating System :: OS Independent",
     ],
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
