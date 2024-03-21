8ff098b45561bf7b24142846033f9eec8960c467
Christian Ledermann <christian.ledermann@gmail.com>
2024-03-21 15:43:14 +0000

    remove redundant tests

--- tests/hypothesis/test_point.py
+++ tests/hypothesis/test_point.py
@@ -12,20 +12,6 @@ from pygeoif.hypothesis.strategies import points
 
 
 @given(point_coords(epsg4326))
-def test_repr_eval_epsg_4326(point) -> None:
-    point = geometry.Point(*point)
-
-    assert eval(repr(point), {}, {"Point": geometry.Point}) == point
-
-
-@given(point_coords(epsg4326))
-def test_shape_epsg_4326(point) -> None:
-    point = geometry.Point(*point)
-
-    assert point == shape(point)
-
-
-@given(point_coords(epsg4326))
 def test_from_wkt_epsg_4326(point) -> None:
     point = geometry.Point(*point)
 
