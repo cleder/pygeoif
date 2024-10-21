8b04d23ea835efaf2bd41765c1939676fbcd3fef
Christian Ledermann <christian.ledermann@gmail.com>
2024-10-21 17:47:08 +0100

    Add force_2d and force_3d functions to pygeoif/__init__.py fixes #255

--- pygeoif/__init__.py
+++ pygeoif/__init__.py
@@ -16,7 +16,10 @@
 #   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 """PyGeoIf provides a GeoJSON-like protocol for geo-spatial (GIS) vector data."""
+
 from pygeoif.about import __version__  # noqa: F401
+from pygeoif.factories import force_2d
+from pygeoif.factories import force_3d
 from pygeoif.factories import from_wkt
 from pygeoif.factories import mapping
 from pygeoif.factories import orient
@@ -43,6 +46,8 @@ __all__ = [
     "MultiPolygon",
     "Point",
     "Polygon",
+    "force_2d",
+    "force_3d",
     "from_wkt",
     "mapping",
     "orient",
